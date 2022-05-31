2415baad43028e56abaef955bfa930eadef623a8
Rozpi <joserozpide@yahoo.es>
2022-05-31 21:25:03 +0000

    todo-list-api310522

---
+++ src/api/task.api.js
@@ -0,0 +1,14 @@
+
+import helper from "../helper.js"
+
+const taskApi = {
+    update: (username, body) => {
+    return helper.request({
+            method: 'PUT',
+            param: username,
+            body,
+        })
+    },
+}
+
+export default taskApi;
